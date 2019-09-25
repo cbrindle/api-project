b0182a8f6810bb78e32acde4190613b62edb02ad
Christopher Brindle <chris.brindle@codeimmersives.com>
2019-09-25 16:26:12 -0400

    Initial Code Commenting Added code comments to better explain the functionality of JS code

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
+// Function that runs when "Find Target" button is clicked
 function getRickAndMortyData(num = 0) {
     event.preventDefault();
+    
+    // Clears any DOM data in #t-results-area
     clearData();
+
+    // Get the value of what the user input in the #char-name box
     const charName = document.querySelector(`#char-name`).value;
     document.querySelector(`#char-name`).disabled = true;
 
+    // Create new XHTML request targeting the API's URL
     let xhttp = new XMLHttpRequest;
     let url = `https://rickandmortyapi.com/api/character/?name=${charName}`
 
     xhttp.open('GET', url, true);
     xhttp.send();
 
+    // Get data from API
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             parseObj = JSON.parse(this.responseText);
             resultSize = parseObj.results.length;
             console.log(parseObj);
 
-            if (resultSize > 1) {
+            // Display numbers to click for additional results
+            if (resultSize > 1 && resultSize <= 20) {
                 for (let i = 0; i < resultSize; i++) {
                     console.log([i]);
                     const newLink = document.createElement(`a`);
@@ -27,6 +35,7 @@ function getRickAndMortyData(num = 0) {
                 }
             }
 
+            // Display information from parsed JSON object
             document.querySelector(`#target-search-name`).innerText = `Search Results For: '${charName}'`
 
             document.querySelector(`#main-right-image-area`).style.backgroundImage = `url(${parseObj.results[num].image})`;
@@ -46,16 +55,19 @@ function getRickAndMortyData(num = 0) {
     }
 }
 
+// Function to run when a result number is clicked
 function changeResult(event) {
     clearData();
     const num = Number(event.target.innerText) - 1;
     getRickAndMortyData(num);
 }
 
+// Clears DOM data in #target-result-area
 function clearData() {
     document.querySelector(`#t-results`).innerHTML = ``;
 }
 
+// Enables #char-name input box
 function newSearch() {
     clearData();
     document.querySelector(`#char-name`).disabled = false;
